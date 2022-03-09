af5180c668caaf8081b42731b0c6eb65eba932dd
Mashrur Shuvo <Mashrur.Shuvo@gmail.com>
2022-03-09 12:57:19 -0500

    Initial commit

---
+++ test-script.py
@@ -0,0 +1,14 @@
+import sys
+import requests
+
+# print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = f"Hello, {who_to_greet}"
+    return greeting
+
+
+r = requests.get('https://mashrurshuvo.wordpress.com/')
+print(r.status_code)
